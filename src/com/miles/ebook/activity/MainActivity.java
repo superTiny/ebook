d1b76a2bb77e1f9bad6b38762b06eb4a0efcf21c
superTiny <1632686004@qq.com>
2014-10-20 19:10:50 +0800

    提交欢迎页

--- src/com/miles/ebook/activity/MainActivity.java
+++ src/com/miles/ebook/activity/MainActivity.java
@@ -1,18 +1,32 @@
 package com.miles.ebook.activity;
 
+import java.util.logging.Logger;
+
 import com.miles.ebook.R;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends Activity
 {
+
+	private static final String TAG = "MainActivity";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		TextView tv=(TextView) findViewById(R.id.tv);
+		String a=(Intent.FLAG_ACTIVITY_NO_ANIMATION
+				| Intent.FLAG_ACTIVITY_CLEAR_TOP)+"";
+		tv.setText(a);
+		Log.e(TAG, a);
+		Log.e(TAG, Integer.toHexString(Intent.FLAG_ACTIVITY_NO_ANIMATION
+				| Intent.FLAG_ACTIVITY_CLEAR_TOP));
 	}
 
 }
--- src/com/miles/ebook/activity/start/WelcomActivity.java
+++ src/com/miles/ebook/activity/start/WelcomActivity.java
@@ -2,31 +2,51 @@ package com.miles.ebook.activity.start;
 
 import com.miles.ebook.R;
 import com.miles.ebook.activity.MainActivity;
+import com.nineoldandroids.animation.ObjectAnimator;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
 
 public class WelcomActivity extends Activity
 {
+
+	ImageView mImageView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.welcom_main_layout);
+		mImageView = (ImageView) findViewById(R.id.image);
+		mImageView.setOnClickListener(new OnClickListener()
+		{
+
+			@Override
+			public void onClick(View v)
+			{
+				goToMainActivity();
+			}
+		});
 	}
-	
+
 	@Override
 	protected void onResume()
 	{
 		super.onResume();
-		goToMainActivity();
 	}
 
 	private void goToMainActivity()
 	{
-		
-		startActivity(new Intent(WelcomActivity.this,MainActivity.class));
+//		ObjectAnimator.ofFloat(mImageView, "alpha", 1, 0, 1).setDuration(15000)
+//				.start();
+		Intent intent = new Intent(WelcomActivity.this, MainActivity.class);
+		intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION
+				| Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		startActivity(intent);
+		overridePendingTransition(R.anim.activity_in, R.anim.activity_out);
 	}
 }
